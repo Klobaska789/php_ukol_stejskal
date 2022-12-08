7b3ac2992eec3c04de8e368b4169b3364820b7dd
Merci <86232880+Klobaska789@users.noreply.github.com>
2022-12-08 20:15:12 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -44,33 +44,33 @@
      <?php
        if(isset($_POST['prvni'])){$ipnumber = $_POST['text1'];
       list($ip1, $ip2, $ip3, $ip4) = explode(".", "$ipnumber");
-      print(decbin("$ip1")); echo " ----- ";
-      print(dechex("$ip1"));echo "<br>";
-      print(decbin("$ip2")); echo " ----- ";
-      print(dechex("$ip2"));echo "<br>";
-      print(decbin("$ip3")); echo " ----- ";
-      print(dechex("$ip3")); echo "<br>";
-      print(decbin("$ip4")); echo " ----- ";
+      print(decbin("$ip1")); echo ",";
+      print(decbin("$ip2")); echo ",";
+      print(decbin("$ip3")); echo ",";
+      print(decbin("$ip4")); echo "<br>";
+      print(dechex("$ip1"));echo ":";
+      print(dechex("$ip2"));echo ":";
+      print(dechex("$ip3"));echo ":";
       print(dechex("$ip4"));}
       if(isset($_POST['druhy'])){$binnum = $_POST['text2'];
       list($bin1, $bin2, $bin3, $bin4) = explode(",", "$binnum");
-      print(bindec("$bin1"));echo " ----- ";
-      printf(dechex(bindec($bin1)));echo "<br>";
-      print(bindec("$bin2"));echo " ----- ";
-      printf(dechex(bindec($bin2)));echo "<br>";
-      print(bindec("$bin3"));echo " ----- ";
-      printf(dechex(bindec($bin3)));echo "<br>";
-      print(bindec("$bin4"));echo " ----- ";
-      printf(dechex(bindec($bin4)));;}
+      print(bindec("$bin1"));echo ".";
+      print(bindec("$bin2"));echo ".";
+      print(bindec("$bin3"));echo ".";
+      print(bindec("$bin4"));echo "<br>";
+      printf(dechex(bindec($bin1)));echo ":";
+      printf(dechex(bindec($bin2)));echo ":";
+      printf(dechex(bindec($bin3)));echo ":";
+      printf(dechex(bindec($bin4)));}
       if(isset($_POST['treti'])){$hexnum = $_POST['text3'];
-      list($hex1, $hex2, $hex3, $hex4) = explode(",", "$hexnum");
-      print(hexdec("$hex1"));echo " ----- ";
-      printf(decbin(hexdec($hex1)));echo "<br>";
-      print(hexdec("$hex2"));echo " ----- ";
-      printf(decbin(hexdec($hex2)));echo "<br>";
-      print(hexdec("$hex3"));echo " ----- ";
-      printf(decbin(hexdec($hex3)));echo "<br>";
-      print(hexdec("$hex4"));echo " ----- ";
-      printf(decbin(hexdec($hex4)));echo "<br>";}?> 
+      list($hex1, $hex2, $hex3, $hex4) = explode(":", "$hexnum");
+      print(hexdec("$hex1"));echo ".";
+      print(hexdec("$hex2"));echo ".";
+      print(hexdec("$hex3"));echo ".";
+      print(hexdec("$hex4"));echo "<br>";
+      printf(decbin(hexdec($hex1)));echo ",";
+      printf(decbin(hexdec($hex2)));echo ",";
+      printf(decbin(hexdec($hex3)));echo ",";
+      printf(decbin(hexdec($hex4)));}?> 
 </body>
 </html>
